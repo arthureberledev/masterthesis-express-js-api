15caa12804a910d156c07248f69f4ac7cc77dd00
arthureberle <58264696+arthureberle@users.noreply.github.com>
2023-09-29 17:25:16 +0200

    added self signed ssl certificates

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const app = require("./app");
+const https = require("https");
+const fs = require("fs");
+
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
+const options = {
+  cert: fs.readFileSync("./ssl/cert.pem"),
+  key: fs.readFileSync("./ssl/key.pem"),
+  passphrase: "password123",
+};
+
+https.createServer(options, app).listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
